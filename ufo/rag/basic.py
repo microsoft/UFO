bc813e420d71d493e50c7e85bf482c50b826096b
vyokky <7678676@qq.com>
2024-03-03 21:09:26 +0800

    get config

--- ufo/rag/basic.py
+++ ufo/rag/basic.py
@@ -1,3 +1,6 @@
+import os
+import json
+
 class Retriever:
     """
     Class to retrieve documents.
@@ -16,3 +19,35 @@ class Retriever:
         :return: The document from the given query.
         """
         return self.indexer.similarity_search(query, top_k, filter=filter)
+
+
+
+
+def load_json_file(file_path):
+    """
+    Load a JSON file.
+    :param file_path: The path to the file to load.
+    :return: The loaded JSON data.
+    """
+
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+
+def get_offline_config():
+    """
+    Get the list of offline indexers.
+    :return: The list of offline indexers.
+    """
+
+    if os.path.exists("learner/records.json"):
+        records = load_json_file("./learner/records.json")
+    else:
+        records = {}
+    return records
+
+
+
+
+    
