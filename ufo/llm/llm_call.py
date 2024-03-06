1d28b1f8afdccc7d0db6b9f4dbebe0eb14ab5e64
Mac0q <gmacq0209@gmail.com>
2024-03-06 13:13:33 +0800

    update AAD login

--- ufo/llm/llm_call.py
+++ ufo/llm/llm_call.py
@@ -5,6 +5,8 @@ import requests
 import time
 from ..config.config import load_config
 from ..utils import print_with_color
+from .azure_ad import get_chat_completion
+
 
 configs = load_config()
 
@@ -21,34 +23,50 @@ def get_gptv_completion(messages, headers):
     max_retry: The maximum number of retries.
     return: The response of the request.
     """
-
-    payload = {
-        "messages": messages,
-        "temperature": configs["TEMPERATURE"],
-        "max_tokens": configs["MAX_TOKENS"],
-        "top_p": configs["TOP_P"],
-        "model": configs["OPENAI_API_MODEL"]
-    }
-
+    aad = configs['API_TYPE'].lower() == 'azure_ad'
+    if not aad:
+        payload = {
+            "messages": messages,
+            "temperature": configs["TEMPERATURE"],
+            "max_tokens": configs["MAX_TOKENS"],
+            "top_p": configs["TOP_P"],
+            "model": configs["OPENAI_API_MODEL"]
+        }
 
     for _ in range(configs["MAX_RETRY"]):
         try:
-            response = requests.post(configs["OPENAI_API_BASE"], headers=headers, json=payload)
-            response_json = response.json()
-            response.raise_for_status()  # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
-            
+            if not aad :
+                response = requests.post(configs["OPENAI_API_BASE"], headers=headers, json=payload)
 
-            if "choices" not in response_json:
-                print_with_color(f"GPT Error: No Reply", "red")
-                continue
+                response_json = response.json()
+                response.raise_for_status()  # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
+                
 
-            if "error" not in response_json:
-                usage = response_json.get("usage", {})
-                prompt_tokens = usage.get("prompt_tokens", 0)
-                completion_tokens = usage.get("completion_tokens", 0)
+                if "choices" not in response_json:
+                    print_with_color(f"GPT Error: No Reply", "red")
+                    continue
+
+                if "error" not in response_json:
+                    usage = response_json.get("usage", {})
+                    prompt_tokens = usage.get("prompt_tokens", 0)
+                    completion_tokens = usage.get("completion_tokens", 0)
+            else:
+                response = get_chat_completion(
+                    engine=configs["OPENAI_API_MODEL"],
+                    messages = messages,
+                    max_tokens = configs["MAX_TOKENS"],
+                    temperature = configs["TEMPERATURE"],
+                    top_p = configs["TOP_P"],
+                )
                 
-                cost = prompt_tokens / 1000 * 0.01 + completion_tokens / 1000 * 0.03
-                
+                if "error" not in response:
+                    usage = response.usage
+                    prompt_tokens = usage.prompt_tokens
+                    completion_tokens = usage.completion_tokens
+                response_json = response
+
+            cost = prompt_tokens / 1000 * 0.01 + completion_tokens / 1000 * 0.03
+             
             return response_json, cost
         except requests.RequestException as e:
             print_with_color(f"Error making API request: {e}", "red")
@@ -59,4 +77,3 @@ def get_gptv_completion(messages, headers):
                 _ 
             time.sleep(3)
             continue
-
