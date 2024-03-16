7c61c0d75f06b42a82dae369cf20b9f660c62bf0
Mac0q <gmacq0209@gmail.com>
2024-03-16 14:34:52 +0800

    backup format

--- ufo/llm/llm_call.py
+++ ufo/llm/llm_call.py
@@ -8,7 +8,7 @@ from ..config.config import load_config
 configs = load_config()
 
 
-def get_completion(messages, is_visual=True, agent: str='APP', backup_engine: bool=True):
+def get_completion(messages, is_visual=True, agent: str='APP', use_backup_engine: bool=True):
     """
     Get completion for the given messages.
 
@@ -16,7 +16,7 @@ def get_completion(messages, is_visual=True, agent: str='APP', backup_engine: bo
         messages (list): List of messages to be used for completion.
         is_visual (bool, optional): Flag indicating whether the completion is visual or not. 
         agent (str, optional): Type of agent. Possible values are 'APP', 'ACTION' or 'BACKUP'.
-        backup_engine (bool, optional): Flag indicating whether to use the backup engine or not.
+        use_backup_engine (bool, optional): Flag indicating whether to use the backup engine or not.
 
     Returns:
         tuple: A tuple containing the completion response (str) and the cost (float).
@@ -32,16 +32,16 @@ def get_completion(messages, is_visual=True, agent: str='APP', backup_engine: bo
         raise ValueError(f'Agent {agent} not supported')
     
     api_type =  configs[agent_type]['VISUAL' if is_visual else 'NON_VISUAL']['API_TYPE']
-    if api_type.lower() in ['openai', 'aoai', 'azure_ad']:
-        from .openai import OpenAIService
-        try:
+    try:
+        if api_type.lower() in ['openai', 'aoai', 'azure_ad']:
+            from .openai import OpenAIService
             response, cost = OpenAIService(configs, is_visual=is_visual, agent_type=agent_type).chat_completion(messages)
-        except Exception as e:
-            if backup_engine:
-                print_with_color(f"OpenAI API request failed: {e}, try to use the backup engine", "red")
-                return get_completion(messages, is_visual=is_visual, agent='backup', backup_engine=False)
-            else:
-                raise e
-        return response, cost
-    else:
-        raise ValueError(f'API_TYPE {api_type} not supported')
+            return response, cost
+        else:
+            raise ValueError(f'API_TYPE {api_type} not supported')
+    except Exception as e:
+        if use_backup_engine:
+            print_with_color(f"The API request of {agent_type} failed: {e}, try to use the backup engine", "red")
+            return get_completion(messages, is_visual=is_visual, agent='backup', use_backup_engine=False)
+        else:
+            raise e
