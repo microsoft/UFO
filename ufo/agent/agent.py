b4a87e2e361da539af6411488b6f34123b6d67af
Mac0q <gmacq0209@gmail.com>
2024-05-11 18:22:35 +0800

    fix

--- ufo/agent/agent.py
+++ ufo/agent/agent.py
@@ -522,7 +522,7 @@ class HostAgent(BasicAgent):
         )
         utils.print_with_color("Status📊: {status}".format(status=status), "blue")
         utils.print_with_color(
-            "Next Plan📚: {plan}".format(plan="\n".join(plan)), "cyan"
+            "Next Plan📚: {plan}".format(plan=str(plan) if isinstance(plan[0], dict) else "\n".join(plan)), "cyan"
         )
         utils.print_with_color("Comment💬: {comment}".format(comment=comment), "green")
 
