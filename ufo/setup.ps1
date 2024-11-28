17c2fdee7c91decfbdfbd325099211248530cdb1
Paul Jiang <zhaojia@microsoft.com>
2024-11-28 15:21:15 +0800

    Add logs

--- ufo/setup.ps1
+++ ufo/setup.ps1
@@ -5,18 +5,25 @@ Import-Module (Join-Path $scriptFolder -ChildPath "setup-tools.psm1")
 
 # Initialize the script folder to the directory of the current script
 $requirementsFile = "$PSScriptRoot\requirements.txt"
+$outputFile = "$PSScriptRoot\Logs.txt"
 
 # Ensure pip is updated to the latest version
-Install-PythonPackages -Package "pip" -Arguments "--upgrade"
+try {
+    Install-PythonPackages -Package "pip" -Arguments "--upgrade"
+    Install-PythonPackages -Package "wheel"
+    Install-PythonPackages -Package "pywinauto"
 
-Install-PythonPackages -Package "wheel"
-Install-PythonPackages -Package "pywinauto"
+    # Install Python packages from requirements.txt using Python's pip module
+    if (Test-Path $requirementsFile) {
+        Write-Host "Installing required Python packages using pip from requirements file..."
+        Install-PythonPackages -RequirementsPath $requirementsFile
 
-# Install Python packages from requirements.txt using Python's pip module
-if (Test-Path $requirementsFile) {
-    Write-Host "Installing required Python packages using pip from requirements file..."
-    Install-PythonPackages -RequirementsPath $requirementsFile
-} else {
-    Write-Error "Requirements file not found: $requirementsFile"
-    exit
+        # Write a text file indicating that the setup is finished
+        "Setup finished" | Out-File $outputFile
+    } else {
+        throw "Requirements file not found: $requirementsFile"
+    }
+} catch {
+    # Log the error message to the output file
+    $_.Exception.Message | Out-File $outputFile
 }
