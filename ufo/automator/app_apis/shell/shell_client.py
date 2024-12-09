99f42ae2e17cf198f664378261af64749f00449a
vyokky <7678676@qq.com>
2024-12-09 13:50:06 +0800

    shell

---
+++ ufo/automator/app_apis/shell/shell_client.py
@@ -0,0 +1,86 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+from __future__ import annotations
+
+import subprocess
+from typing import Any, Dict, Type
+
+from ufo.automator.basic import CommandBasic, ReceiverBasic
+
+
+class ShellReceiver(ReceiverBasic):
+    """
+    The base class for Web COM client using crawl4ai.
+    """
+
+    _command_registry: Dict[str, Type[ShellCommand]] = {}
+
+    def __init__(self) -> None:
+        """
+        Initialize the shell client.
+        """
+
+    def run(self, params: Dict[str, Any]) -> Any:
+        """
+        Run the command.
+        :param params: The parameters of the command.
+        :return: The result content.
+        """
+        bash_command = params.get("command")
+        result = subprocess.run(
+            bash_command, shell=True, capture_output=True, text=True
+        )
+        return result.stdout
+
+    @property
+    def type_name(self):
+        return "SHELL"
+
+    @property
+    def xml_format_code(self) -> int:
+        return 0  # This might not be applicable for web, adjust accordingly
+
+
+class ShellCommand(CommandBasic):
+    """
+    The base class for Web commands.
+    """
+
+    def __init__(self, receiver: ShellReceiver, params: Dict[str, Any]) -> None:
+        """
+        Initialize the Web command.
+        :param receiver: The receiver of the command.
+        :param params: The parameters of the command.
+        """
+        super().__init__(receiver, params)
+        self.receiver = receiver
+        self.params = params
+
+    @classmethod
+    def name(cls) -> str:
+        """
+        The name of the command.
+        """
+        return "shell"
+
+
+@ShellReceiver.register
+class RunShellCommand(ShellCommand):
+    """
+    The command to run the crawler with various options.
+    """
+
+    def execute(self):
+        """
+        Execute the command to run the crawler.
+        :return: The result content.
+        """
+        return self.receiver.run(params=self.params)
+
+    @classmethod
+    def name(cls) -> str:
+        """
+        The name of the command.
+        """
+        return "shell_run"
