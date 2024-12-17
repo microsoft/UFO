6632e95df8b38bde9df9d360c742d58ec312b670
Paul Jiang <zhaojia@microsoft.com>
2024-12-17 10:28:40 +0800

    Fix Merge issue

--- ufo/ufo.py
+++ ufo/ufo.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from ufo.config.config import Config
 from ufo.module.client import UFOClientManager
 from ufo.module.sessions.session import SessionFactory
+import json
 
 configs = Config.get_instance().config_data
 
@@ -40,46 +41,14 @@ args.add_argument(
     default="",
 )
 args.add_argument(
-    "--instruction",
-    "-i",
-    help="The payload",
-    type=str,
-    default="",
-)
-args.add_argument(
     "--agent_settings",
     "-a",
     help="The agent settings.",
     type=str,
     default="",
 )
-args.add_argument(
-    "--run_arena",
-    "-r",
-    help="The payload",
-    type=bool,
-    default=False,
-)
 
 parsed_args = args.parse_args()
-
-from typing import Dict, List
-import requests
-from uuid import uuid4
-import json
-import os
-import argparse
-
-from ufo.module.sessions.service_session import ServiceSession
-from ufo.config.config import Config
-
-session_dir = "sessions"
-
-# if os.path.exists("\\\\host.lan\\Data"):
-#     session_dir = f"\\\\host.lan\\Data\\server_outputs\\sessions"
-
-if not os.path.exists(session_dir):
-    os.makedirs(session_dir)
 
 def main():
     """
@@ -95,60 +64,27 @@ def main():
     python -m ufo -t task_name -m batch_normal -p path_to_plan_file_or_folder
     """
 
-    if (parsed_args.run_arena):
-        run_ufo_session(instruction=parsed_args.instruction, agent_settings=parsed_args.agent_settings)
-    else:
-        sessions = SessionFactory().create_session(
-            task=parsed_args.task,
-            mode=parsed_args.mode,
-            plan=parsed_args.plan,
-            request=parsed_args.request,
-        )
-
-        clients = UFOClientManager(sessions)
-        clients.run_all()
-
-def run_ufo_session(instruction: str, agent_settings: str = "") -> Dict:
-    print("Running UFO session")
-
-    try:
-        agent_settings_json = json.loads(agent_settings)
+    # Set config if agent setting exist.
+    if parsed_args.agent_settings is not None and parsed_args.agent_settings != "":
+        try:
+            agent_settings_json = json.loads(parsed_args.agent_settings)
+        except json.JSONDecodeError:
+            agent_settings_json = {}
+        
         set_config(agent_settings_json)
+    
+    sessions = SessionFactory().create_session(
+        task=parsed_args.task,
+        mode=parsed_args.mode,
+        plan=parsed_args.plan,
+        request=parsed_args.request,
+    )
 
-        session_id = str(uuid4())
-        session = ServiceSession(session_id, configs.get("EVA_SESSION", False), id=0)
-        session._request = instruction
-        session.run()
-
-        # save the session information to log
-        session_info = {
-            "session_id": session_id,
-            "instruction": instruction,
-            "status": "success"
-        }
-
-        session_file = os.path.join(session_dir, f"{session_id}.json")
-        with open(session_file, "w") as f:
-            json.dump(session_info, f)
-
-    except Exception as e:
-        print(f"Error running UFO session: {e}")
-
-        # save the session information to log
-        session_info = {
-            "session_id": session_id,
-            "instruction": instruction,
-            "status": "error",
-            "error": str(e),
-        }
-
-        session_file = os.path.join(session_dir, f"{session_id}.json")
-        with open(session_file, "w") as f:
-            json.dump(session_info, f)
+    clients = UFOClientManager(sessions)
+    clients.run_all()
 
 def set_config(agent_settings_json: dict = {}):
     if agent_settings_json is not None and len(agent_settings_json) > 0:
-        
         llm_type = agent_settings_json.get("llm_type")
         llm_endpoint = agent_settings_json.get("llm_endpoint")
         llm_auth = agent_settings_json.get("llm_auth", {})
@@ -184,10 +120,6 @@ def set_config(agent_settings_json: dict = {}):
         configs["HOST_AGENT"]["API_MODEL"] = "gpt-4-vision-preview"
         configs["APP_AGENT"]["API_MODEL"] = "gpt-4-vision-preview"
         configs["BACKUP_AGENT"]["API_MODEL"] = "gpt-4-vision-preview"
-
-        print("config seeting" + configs["HOST_AGENT"]["API_TYPE"])
-        print("config Key" + configs["HOST_AGENT"]["API_KEY"])
-        print("config Endpoint   " + configs["HOST_AGENT"]["API_BASE"])
 
 if __name__ == "__main__":
     main()
