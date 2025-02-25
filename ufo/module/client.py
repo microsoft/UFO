28cc256c90aac1eb17f9726d651f26e5255811c6
2020-qqtcg <mj1921761583@gmail.com>
2025-02-25 15:12:23 +0800

    add blob

--- ufo/module/client.py
+++ ufo/module/client.py
@@ -1,13 +1,16 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
-
-
+import smtplib
+from email.header import Header
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from typing import List
 
 from tqdm import tqdm
 
 from ufo.config.config import Config
 from ufo.module.basic import BaseSession
+from ufo.utils.azure_storage import AzureBlobStorage
 
 _configs = Config.get_instance().config_data
 
@@ -27,19 +30,23 @@ class UFOClientManager:
         """
         Run the batch UFO client.
         """
-        if _configs["MONITOR"]:
-            send_point = _configs["SEND_POINT"].split(",")
-            total = len(self.session_list)
+        blob_storage = None
+        if _configs["UPLOAD"]:
+            blob_storage = AzureBlobStorage()
 
-            for idx, session in enumerate(tqdm(self.session_list), start=1):
-                session.run()
+        total = len(self.session_list)
+        for idx, session in enumerate(tqdm(self.session_list), start=1):
+            session.run()
 
+            if _configs["MONITOR"]:
+                send_point = _configs["SEND_POINT"].split(",")
                 if str(idx) in send_point:
                     message = f"Ufo Execute Completed: {idx}/{total}"
                     send_message(message)
-        else:
-            for session in tqdm(self.session_list):
-                session.run()
+
+            if _configs["UPLOAD"] and idx % _configs["UPLOAD_INTERVAL"] == 0:
+                blob_storage.upload_folder(_configs["LOG_ROOT"], _configs["DATA_SOURCE"])
+
 
     @property
     def session_list(self) -> List[BaseSession]:
@@ -62,3 +69,40 @@ class UFOClientManager:
         :return: The next session.
         """
         return self._session_list.pop(0)
+
+def send_message(message: str) -> None:
+    """
+    Send a message.
+    :param message: message to send.
+    """
+    # email info
+    sender_email = _configs["FROM_EMAIL"]
+    sender_password = _configs["SENDER_PASSWORD"]
+    receiver_email = _configs["TO_EMAIL"]
+    server_host = _configs["SMTP_SERVER"]
+    machine_id = _configs["MACHINE_ID"]
+
+    msg = MIMEMultipart()
+    msg["From"] = _configs["FROM_EMAIL"]
+    msg["TO"] = _configs["TO_EMAIL"]
+    msg["Subject"] = Header(f"Execute Reminder: {machine_id}")
+    msg.attach(MIMEText(message, "plain", 'utf-8'))
+
+    server = None
+    try:
+        # Connect SMTP Server
+        server = smtplib.SMTP(server_host, 587)
+        server.starttls()
+        server.login(sender_email, sender_password)
+
+        # send email
+        server.sendmail(sender_email, receiver_email, msg.as_string())
+        print(f"Send Email to {receiver_email} sucessfully.")
+    except Exception as e:
+        print(f"Send Email to {receiver_email} failed: {e}.")
+    finally:
+        try:
+            if server:
+                server.quit()
+        except Exception:
+            return
