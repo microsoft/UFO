ac416d84cfcc017ab015200333551b326463443d
Chiming Ni <v-chimingni@microsoft.com>
2025-03-11 14:03:41 +0800

    Revert "Merge branch 'dev/waa' into vyokky/operator"
    This reverts commit ea7341baecc0fbf98c871f3e45386580dd5dad20, reversing
    changes made to e7c33ffe47cf756b780955fb3b9f8a37ab6c9a53.

--- windows_arena/__main__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-from windows_arena import windows_arena
-
-if __name__ == "__main__":
-    # Execute the main script
-    windows_arena.main()
