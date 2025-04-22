79b79c830ff4de8eeb4d3bac1f170d3d3d57f4fb
Chiming Ni <v-chimingni@microsoft.com>
2025-04-22 11:07:56 +0800

    Revert "Modify grounding service to comply with `omniparserserver` provided by OmniParser repo"
    This reverts commit bb68eaaa904b690cb4dc2cdcc4b065bd6eb4be4e.

--- ufo/llm/grounding_model/omniparser_service.py
+++ ufo/llm/grounding_model/omniparser_service.py
@@ -2,8 +2,8 @@
 # Licensed under the MIT License.
 
 from ufo.llm.base import BaseService
-import requests
-import base64
+from gradio_client import Client, handle_file
+
 
 class OmniParser(BaseService):
     """
@@ -15,50 +15,28 @@ class OmniParser(BaseService):
         Initialize the OmniParser service.
         :param endpoint: The endpoint address of the OmniParser service.
         """
-        try:
-            probe_response = requests.get(f"{endpoint}/probe/")
-            probe_response.raise_for_status()
-            print("Connected to Omniparser server successfully.")
-        except requests.exceptions.RequestException as e:
-            print(f"Error connecting to server: {e}")
-            return 1
-        self.endpoint = endpoint
+        self.client = Client(endpoint)
 
     def chat_completion(
         self,
         image_path: str,
+        box_threshold: float = 0.05,
+        iou_threshold: float = 0.1,
+        use_paddleocr: bool = True,
+        imgsz: int = 640,
+        api_name: str = "/process",
     ):
         """
         Get the chat completion from the OmniParser service.
         :param text: The input text.
         :return: The chat completion.
         """
-        try:
-            base64_image = self.encode_image_to_base64(image_path)
-        except Exception as e:
-            print(f"Error reading image file: {e}")
-            return 1
-        
-        # Prepare request payload
-        payload = {
-            "base64_image": base64_image
-        }
-
-        parse_endpoint = f"{self.endpoint}/parse/"
-
-        try:
-            response = requests.post(parse_endpoint, json=payload)
-            response.raise_for_status()
-            
-            # Process response
-            result = response.json()
-        except requests.exceptions.RequestException as e:
-            print(f"Error connecting to server: {e}")
-            return None
-
-        return result
-    
-    def encode_image_to_base64(self, image_path):
-        with open(image_path, 'rb') as image_file:
-            encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
-        return encoded_string
+        results = self.client.predict(
+            image_input=handle_file(filepath_or_url=image_path),
+            box_threshold=box_threshold,
+            iou_threshold=iou_threshold,
+            use_paddleocr=use_paddleocr,
+            imgsz=imgsz,
+            api_name=api_name,
+        )
+        return results
