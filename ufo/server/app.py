47076c733f436e8f170341bdba8787d5c902c0ab
vyokky <7678676@qq.com>
2025-08-13 20:35:51 +0800

    clean code

--- ufo/server/app.py
+++ ufo/server/app.py
@@ -7,7 +7,6 @@ from ufo.server.services.session_manager import SessionManager
 from ufo.server.services.task_manager import TaskManager
 from ufo.server.services.ws_manager import WSManager
 from ufo.server.ws.handler import UFOWebSocketHandler
-from ufo.server.shared import SharedEventLoop
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 import uvicorn, json
 
@@ -32,14 +31,7 @@ app = FastAPI()
 session_manager = SessionManager()
 task_manager = TaskManager()
 ws_manager = WSManager()
-shared_event_loop = SharedEventLoop()
 
-
-# # Create API blueprint
-# api_bp = create_api_blueprint(
-#     session_manager, task_manager, ws_manager, shared_event_loop
-# )
-# app.register_blueprint(api_bp)
 
 # Create API router
 api_router = create_api_router(session_manager, task_manager, ws_manager)
@@ -70,39 +62,6 @@ async def websocket_endpoint(websocket: WebSocket):
     finally:
         if client_id:
             ws_handler.ws_manager.remove_client(client_id)
-
-
-def run_flask(port: int = 5000) -> None:
-    """
-    Run the Flask application on the specified port.
-    :param port: Port number for the Flask application.
-    """
-
-    app.run(host="0.0.0.0", port=port, debug=True, use_reloader=False)
-
-
-def run_ws(port: int = 8765, ws_handler: UFOWebSocketHandler = ws_handler) -> None:
-    """Run the WebSocket server on the specified port.
-    This function initializes a new event loop and starts the WebSocket server.
-    :param port: Port number for the WebSocket server.
-    :param ws_handler: The WebSocket handler instance to handle incoming connections.
-    """
-    global ws_event_loop
-    ws_event_loop = asyncio.new_event_loop()
-    shared_event_loop.loop = ws_event_loop
-    asyncio.set_event_loop(ws_event_loop)
-    import websockets
-
-    ws_server = websockets.serve(
-        ws_handler,
-        "0.0.0.0",
-        port,
-        ping_interval=100,
-        ping_timeout=100,
-    )
-    ws_event_loop.run_until_complete(ws_server)
-    logger.info(f"WebSocket server started on :0.0.0.0:{port}")
-    ws_event_loop.run_forever()
 
 
 if __name__ == "__main__":
