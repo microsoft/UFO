201e94f75c93bbcd07b7ca8d60238dc08eb75926
vyokky <7678676@qq.com>
2025-08-26 21:38:54 +0800

    clean app

--- ufo/agents/agent/customized_agent.py
+++ ufo/agents/agent/customized_agent.py
@@ -1,12 +1,6 @@
-from ufo.agents.agent.basic import AgentRegistry
-
 from ufo.agents.agent.app_agent import AppAgent
-from ufo.config import Config
-
+from ufo.agents.agent.basic import AgentRegistry
 from ufo.agents.processors.hardware_agent_processor import HardwareAgentProcessor
-
-
-configs = Config.get_instance().config_data
 
 
 @AgentRegistry.register(
@@ -15,7 +9,7 @@ configs = Config.get_instance().config_data
 class HardwareAgent(AppAgent):
     """
     HardwareAgent is a specialized agent that interacts with hardware components.
-    It extends the BasicAgent to provide additional functionality specific to hardware.
+    It extends the AppAgent to provide additional functionality specific to hardware.
     """
 
     pass
