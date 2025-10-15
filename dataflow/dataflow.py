5e81b9be3d2a36894149446fa07985208fbcb102
2020-qqtcg <mj1921761583@gmail.com>
2025-10-15 15:27:20 +0800

    clean

--- dataflow/dataflow.py
+++ dataflow/dataflow.py
@@ -1,19 +1,15 @@
 import argparse
 import os
-import threading
+import smtplib
 import traceback
-from concurrent.futures import ThreadPoolExecutor
+from email.header import Header
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 from tqdm import tqdm
 
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.header import Header
-
-from ufo.utils import print_with_color
-from ufo.utils.azure_storage import AzureBlobStorage
 from dataflow.config.config import Config
+from ufo.utils import print_with_color
 
 _configs = Config.get_instance().config_data
 
@@ -103,63 +99,9 @@ def process_batch(task_dir: str, task_type: str) -> None:
         return
 
     print_with_color(f"Found {len(task_files)} tasks in {task_dir}.", "blue")
-
-    total = len(task_files)
-
     
     for idx, task_file in enumerate(tqdm(task_files), start=1):
         process_task(task_file, task_type)
-
-        if _configs["MONITOR"]:
-            # Send email notify
-            send_point = _configs["SEND_POINT"].split(",")
-            if str(idx) in send_point:
-                message = f"Task Completed {idx}/{total}"
-                send_message(message)
-
-    if _configs["UPLOAD"]:
-        # Upload outputs to azure blob
-        blob_storage = AzureBlobStorage()
-        blob_storage.upload_folder(_configs["OUTPUTS_PATH"], _configs["DATA_SOURCE"], overwrite=False)
-
-
-
-def send_message(message: str) -> None:
-    """
-    Send a message.
-    :param message: message to send.
-    """
-    # email info
-    sender_email = _configs["FROM_EMAIL"]
-    sender_password = _configs["SENDER_PASSWORD"]
-    receiver_email = _configs["TO_EMAIL"]
-    server_host = _configs["SMTP_SERVER"]
-    machine_id = _configs["MACHINE_ID"]
-
-    msg = MIMEMultipart()
-    msg["From"] = _configs["FROM_EMAIL"]
-    msg["TO"] = _configs["TO_EMAIL"]
-    msg["Subject"] = Header(f"Prefill Progress Reminder: {machine_id}")
-    msg.attach(MIMEText(message, "plain", 'utf-8'))
-
-    server = None
-    try:
-        # Connect SMTP Server
-        server = smtplib.SMTP(server_host, 587)
-        server.starttls()
-        server.login(sender_email, sender_password)
-
-        # 发送邮件
-        server.sendmail(sender_email, receiver_email, msg.as_string())
-        print(f"Send Email to {receiver_email} sucessfully.")
-    except Exception as e:
-        print(f"Send Email to {receiver_email} failed: {e}.")
-    finally:
-        try:
-            if server:
-                server.quit()
-        except Exception:
-            return
 
 def main():
     """
--- ufo/module/client.py
+++ ufo/module/client.py
@@ -1,19 +1,9 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
-import smtplib
-import threading
-from concurrent.futures import ThreadPoolExecutor
-from email.header import Header
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from multiprocessing.pool import ThreadPool
 from typing import List
-
-from tqdm import tqdm
 
 from ufo.config.config import Config
 from ufo.module.basic import BaseSession
-from ufo.utils.azure_storage import AzureBlobStorage
 
 _configs = Config.get_instance().config_data
 
@@ -33,24 +23,8 @@ class UFOClientManager:
         """
         Run the batch UFO client.
         """
-        blob_storage = None
-        if _configs["UPLOAD"]:
-            blob_storage = AzureBlobStorage()
-
-        total = len(self.session_list)
-
-        with ThreadPoolExecutor(max_workers=16) as executor:
-            for idx, session in enumerate(tqdm(self.session_list), start=1):
-                session.run()
-
-                if _configs["MONITOR"]:
-                    send_point = _configs["SEND_POINT"].split(",")
-                    if str(idx) in send_point:
-                        message = f"Ufo Execute Completed: {idx}/{total}"
-                        send_message(message)
-
-                if _configs["UPLOAD"]:
-                    executor.submit(lambda : blob_storage.upload_folder(session.log_path, _configs["DATA_SOURCE"]))
+        for session in self.session_list:
+            session.run()
 
 
     @property
@@ -75,39 +49,3 @@ class UFOClientManager:
         """
         return self._session_list.pop(0)
 
-def send_message(message: str) -> None:
-    """
-    Send a message.
-    :param message: message to send.
-    """
-    # email info
-    sender_email = _configs["FROM_EMAIL"]
-    sender_password = _configs["SENDER_PASSWORD"]
-    receiver_email = _configs["TO_EMAIL"]
-    server_host = _configs["SMTP_SERVER"]
-    machine_id = _configs["MACHINE_ID"]
-
-    msg = MIMEMultipart()
-    msg["From"] = _configs["FROM_EMAIL"]
-    msg["TO"] = _configs["TO_EMAIL"]
-    msg["Subject"] = Header(f"Execute Reminder: {machine_id}")
-    msg.attach(MIMEText(message, "plain", 'utf-8'))
-
-    server = None
-    try:
-        # Connect SMTP Server
-        server = smtplib.SMTP(server_host, 587)
-        server.starttls()
-        server.login(sender_email, sender_password)
-
-        # send email
-        server.sendmail(sender_email, receiver_email, msg.as_string())
-        print(f"Send Email to {receiver_email} sucessfully.")
-    except Exception as e:
-        print(f"Send Email to {receiver_email} failed: {e}.")
-    finally:
-        try:
-            if server:
-                server.quit()
-        except Exception:
-            return
